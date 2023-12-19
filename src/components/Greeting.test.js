bd949f3395d620291191b26fd74e865d75574718
Danbi Choi <sweetrain05@hotmail.com>
2023-12-18 17:04:48 -0800

    Group testings using describe method

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,15 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('testing for greeting statement', () => {
-    // Arrange
-    render(<Greeting />);
+describe('Greeting component', () => {
+    test('testing for greeting statement', () => {
+        // Arrange
+        render(<Greeting />);
 
-    // Act
+        // Act
 
-    // Assert
-    const helloWorldText = screen.getByText('Hello world', {
-        exact: false,
+        // Assert
+        const helloWorldText = screen.getByText('Hello world', {
+            exact: false,
+        });
+        expect(helloWorldText).toBeInTheDocument();
     });
-    expect(helloWorldText).toBeInTheDocument();
 });
