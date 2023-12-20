5e347e8f1ec3d5f0818e9b38455cfc4efd8500a6
Danbi Choi <sweetrain05@hotmail.com>
2023-12-19 19:55:34 -0800

    Testing with mock data

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -2,6 +2,10 @@ import { render, screen } from '@testing-library/react';
 import Async from './Async';
 
 test('renders posts if http request succeeds', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+        json: async () => [{ id: 'p1', title: 'First post' }],
+    });
     render(<Async />);
     const listItems = await screen.findAllByRole('listitem');
     expect(listItems).not.toHaveLength(0);
